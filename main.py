7a97b47b436f49b2d702064e6c78b5eb1c256f16
pangliang <418094911@qq.com>
2024-01-02 20:52:16 +0800

    以数组索引作为目标值, 简化功率心率等目标数的设置

--- main.py
+++ main.py
@@ -10,12 +10,15 @@
 # --------------------------------------------------------------
 
 import asyncio
+import pathlib
 import random
 import struct
 import sys
 import os
 import logging
 import time
+from typing import Optional
+import winreg
 
 from bumble.core import AdvertisingData, UUID
 from bumble.device import Device, Connection, DeviceConfiguration
@@ -39,30 +42,43 @@ from bumble.gatt import (
     GATT_BODY_SENSOR_LOCATION_CHARACTERISTIC, DelegatedCharacteristicAdapter, GATT_CYCLING_POWER_SERVICE,
     GATT_CYCLING_SPEED_AND_CADENCE_SERVICE,
 )
+from windows_toasts import AudioSource, Toast, ToastAudio, ToastDisplayImage, WindowsToaster
 
 
 class CyclingSensorsSimulator:
     def __init__(self):
-        self.power_target = 130
-        self.power_target_fn = lambda: random.choices([100, 130, 160, 200, 250, 300], weights=[10, 20, 30, 20, 5, 5])[0]
+        self.running = True
+        self.choise_weights = [1, 3, 5, 3, 1]
+        self.target_index_fn = lambda: random.choices([0, 1, 2, 3, 4], weights=self.choise_weights )[0]
+        self.target_index = self.target_index_fn()
+        self.power_target_choices = [160, 200, 240, 280, 300]
+        self.heart_rate_target_choices = [135, 145, 165, 175, 180]
+
+        self.power_target = self.power_target_choices[0]
         self.power_change = 5
-        self.power_change_fn = lambda: random.randint(5, 30)
-        self.power = 120
-        self.heart_rate_target = 130
-        self.heart_rate_target_fn = lambda power_target: {100: 130, 130: 145, 160: 160, 200: 170, 250: 185, 300: 185}[power_target]
-        self.heart_rate = 100
-        self.heart_rate_change = 2
-        self.heart_rate_change_fn = lambda power_change: 1 + (power_change / 10)
+        self.power_change_fn = lambda: random.randint(1, 3)
+        self.power = self.power_target
+
+        self.heart_rate_target = self.heart_rate_target_choices[0]
+        self.heart_rate = self.heart_rate_target
+        self.heart_rate_change_fn = lambda: random.randint(1, 5)
+        self.heart_rate_change = self.heart_rate_change_fn()
         self.heart_rate_step_length = 5
+
         self.accumulated_torque = 0
         self.accumulated_rpm = 0
         self.last_rpm_update_ts = time.time()
-        self.cadence = 120
-        self.cadence_min = 75
-        self.cadence_max = 95
-        self.cadence_change_fn = lambda: random.randint(2, 8)
+
+        # self.cadence = 80
+        # self.cadence_min = 80
+        # self.cadence_max = 100
+        self.cadence = 60
+        self.cadence_min = 60
+        self.cadence_max = 70
+        self.cadence_change_fn = lambda: random.randint(1, 3)
+
         self.step_length = 0
-        self.step_length_fn = lambda: random.randint(5, 30)
+        self.step_length_fn = lambda: random.randint(3, 15)
         self.battery_level = random.randint(50, 99)  # 每次启动后固定
 
     def loop(self):
@@ -72,8 +88,10 @@ class CyclingSensorsSimulator:
         else:
             # 确定步长
             self.step_length = self.step_length_fn()
+            # 确定目标索引
+            self.target_index = self.target_index_fn()
             # 功率目标
-            self.power_target = self.power_target_fn()
+            self.power_target = self.power_target_choices[self.target_index]
             # 功率变化
             self.power_change = self.power_change_fn()
             logging.info("新的步长:%d, 功率目标:%d, 功率变化:%s", self.step_length, self.power_target, self.power_change)
@@ -85,9 +103,9 @@ class CyclingSensorsSimulator:
             # 确定心率步长
             self.heart_rate_step_length = self.step_length + random.randint(5, 10)
             # 心率目标
-            self.heart_rate_target = self.heart_rate_target_fn(self.power_target)
+            self.heart_rate_target = self.heart_rate_target_choices[self.target_index]
             # 心率变化
-            self.heart_rate_change = self.heart_rate_change_fn(self.power_change)
+            self.heart_rate_change = self.heart_rate_change_fn()
             logging.info("新的心率步长:%d, 心率目标:%d, 心率变化:%s", self.heart_rate_step_length, self.heart_rate_target, self.heart_rate_change)
 
         # 功率
@@ -115,13 +133,13 @@ class CyclingSensorsSimulator:
     # https://bitbucket.org/bluetooth-SIG/public/src/main/gss/org.bluetooth.characteristic.heart_rate_measurement.yaml
     def read_heart_rate(self, connection) -> bytes:
         flags = 0b00
-        data = bytes([flags]) + struct.pack('B', int(self.heart_rate) + random.randint(-5, 5))
+        data = bytes([flags]) + struct.pack('B', int(self.heart_rate) + random.randint(-2, 2))
         return data
 
     # https://bitbucket.org/bluetooth-SIG/public/src/main/gss/org.bluetooth.characteristic.cycling_power_measurement.yaml
     def read_cycling_power(self, connection) -> bytes:
         now = time.time()
-        data = struct.pack('<h', int(self.power + random.randint(-20, 20)))
+        data = struct.pack('<h', int(self.power + random.randint(-5, 5)))
 
         # 有左右平衡
         flags = 0b01
@@ -136,23 +154,21 @@ class CyclingSensorsSimulator:
         self.accumulated_torque %= 0xffff
         data += struct.pack('<H', int(self.accumulated_torque) & 0xffff)
 
-        while now - self.last_rpm_update_ts < 60 / self.cadence:
-            time.sleep(0.1)
-            now = time.time()
-        self.accumulated_rpm += 1  # self.cadence * (now - self.last_rpm_update_ts) / 60
-        self.accumulated_rpm %= 0xffff
-        flags |= 0b100000
-        data += struct.pack('<H', int(self.accumulated_rpm) & 0xffff)
-        data += struct.pack('<H', int(self.last_rpm_update_ts * 1024) & 0xffff)
-        self.last_rpm_update_ts = now
-
+        # while now - self.last_rpm_update_ts < 60 / self.cadence:
+        #     time.sleep(0.1)
+        #     now = time.time()
+        # self.accumulated_rpm += 1  # self.cadence * (now - self.last_rpm_update_ts) / 60
+        # self.accumulated_rpm %= 0xffff
+        # flags |= 0b100000
+        # data += struct.pack('<H', int(self.accumulated_rpm) & 0xffff)
+        # data += struct.pack('<H', int(self.last_rpm_update_ts * 1024) & 0xffff)
+        # self.last_rpm_update_ts = now
 
         data = struct.pack('<H', flags) + data
         return data
 
     # https://bitbucket.org/bluetooth-SIG/public/src/main/gss/org.bluetooth.characteristic.csc_measurement.yaml
     def read_cadence(self, connection) -> bytes:
-
         now = time.time()
         while now - self.last_rpm_update_ts < 60 / self.cadence:
             time.sleep(0.1)
@@ -164,7 +180,7 @@ class CyclingSensorsSimulator:
         data += struct.pack('<H', int(self.accumulated_rpm) & 0xffff)
         data += struct.pack('<H', int(self.last_rpm_update_ts * 1024) & 0xffff)
         self.last_rpm_update_ts = now
-        logging.info("cadence:%d, accumulated_rpm:%f, last_rpm_update_ts:%f", self.cadence, self.accumulated_rpm, self.last_rpm_update_ts)
+        # logging.info("cadence:%d, accumulated_rpm:%f, last_rpm_update_ts:%f", self.cadence, self.accumulated_rpm, self.last_rpm_update_ts)
         return data
 
     def read_battery_level(self, connection) -> int:
@@ -187,7 +203,7 @@ async def main():
                     (AdvertisingData.COMPLETE_LOCAL_NAME, bytes(config.name, 'utf-8')),
                     (AdvertisingData.INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, bytes(GATT_CYCLING_POWER_SERVICE)),
                     (AdvertisingData.INCOMPLETE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS, bytes(GATT_CYCLING_SPEED_AND_CADENCE_SERVICE)),
-                    # (AdvertisingData.APPEARANCE, struct.pack('<H', 0x0340))
+                    (AdvertisingData.APPEARANCE, struct.pack('<H', 0x0340))
                 ]
             )
         )
@@ -360,27 +376,75 @@ async def main():
         await device.power_on()
         await device.start_advertising(auto_restart=True)
 
-        # while True:
-        #     await asyncio.sleep(1)
-        #     simulator.loop()
-        #     await device.notify_subscribers(heart_rate_service.characteristics[0])
-        #     await device.notify_subscribers(cycling_power_service.characteristics[0])
-        #     await device.notify_subscribers(cycling_speed_and_cadence_service.characteristics[0])
+        toaster = WindowsToaster('cycling-sensors-simulator')
+        newToast = Toast()
+        newToast.text_fields = ['程序启动!']
+        toaster.show_toast(newToast)
 
+        start_time = time.time()
+        duration = 65 * 60
         async def main_loop():
-            while True:
-                await asyncio.sleep(1)
-                simulator.loop()
+            while simulator.running:
+                # 如果从开始到现在的时间大于5分钟, 则发送window通知, 并退出程序
+                if time.time() - start_time > duration:
+                    simulator.running = False
+                    return
 
-        async def cadence_loop():
-            while True:
-                await asyncio.sleep(0.2)
+                await asyncio.sleep(0.5)
+                simulator.loop()
                 await device.notify_subscribers(heart_rate_service.characteristics[0])
                 await device.notify_subscribers(cycling_power_service.characteristics[0])
+
+        async def cadence_loop():
+            while simulator.running:
+                await asyncio.sleep(0.3)
                 await device.notify_subscribers(cycling_speed_and_cadence_service.characteristics[0])
 
         await asyncio.gather(main_loop(), cadence_loop())
 
+        await device.stop_advertising()
+        await device.power_off()
+
+        toaster = WindowsToaster('cycling-sensors-simulator')
+        newToast = Toast()
+        newToast.text_fields = ['程序即将退出!']
+        newToast.AddImage(ToastDisplayImage.fromPath(os.path.abspath(os.path.join(os.path.dirname(__file__), "icon.ico"))))
+        newToast.audio = ToastAudio(AudioSource.IM, looping=True)
+        toaster.show_toast(newToast)
+
 # -----------------------------------------------------------------------------
+        
+app_name = "cycling-sensors-simulator"
+def register_hkey(appId: str, appName: str, iconPath: Optional[pathlib.Path]):
+    """
+    Registers an application in the Windows registry.
+
+    :param app_id: The unique ID of the application.
+    :param app_name: The display name of the application.
+    :param icon_path: The path to the application's icon (optional).
+    :raises ValueError: If the icon path does not exist or is not an .ico file.
+    """
+    if iconPath is not None:
+        if not iconPath.exists():
+            raise ValueError(f"Could not register the application: File {iconPath} does not exist")
+        elif iconPath.suffix != ".ico":
+            raise ValueError(f"Could not register the application: File {iconPath} must be of type .ico")
+
+    winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+    keyPath = f"SOFTWARE\\Classes\\AppUserModelId\\{appId}"
+    with winreg.CreateKeyEx(winreg.HKEY_CURRENT_USER, keyPath) as masterKey:
+        winreg.SetValueEx(masterKey, "DisplayName", 0, winreg.REG_SZ, appName)
+        if iconPath is not None:
+            winreg.SetValueEx(masterKey, "IconUri", 0, winreg.REG_SZ, str(iconPath.resolve()))
+
+# 把应用注册到通知中心
+register_hkey(app_name, app_name, pathlib.Path(os.path.abspath(os.path.join(os.path.dirname(__file__), "icon.ico"))))
+
+toaster = WindowsToaster(app_name)
+newToast = Toast()
+newToast.text_fields = ['程序启动!']
+newToast.AddImage(ToastDisplayImage.fromPath(os.path.abspath(os.path.join(os.path.dirname(__file__), "icon.ico"))))
+toaster.show_toast(newToast)
+
 logging.basicConfig(level=os.environ.get('BUMBLE_LOGLEVEL', 'INFO').upper())
 asyncio.run(main())
